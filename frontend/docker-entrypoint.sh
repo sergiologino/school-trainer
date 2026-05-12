09e94d9e39e4de6ad6b1fecb82f75b3fcd2cea2e
Sergey <90287274+sergiologino@users.noreply.github.com>
2026-05-12 13:00:20 +0300

    Add frontend API upstream host variable

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,10 +2,11 @@
 set -eu
 
 : "${NGINX_PORT:=80}"
-: "${API_UPSTREAM:?API_UPSTREAM is required, for example http://school-trainer-backend.localhost.sslip.io}"
+: "${API_UPSTREAM:?API_UPSTREAM is required, for example http://coolify:80}"
+: "${API_UPSTREAM_HOST:?API_UPSTREAM_HOST is required, for example school-trainer-backend.sslip.io}"
 
-export NGINX_PORT API_UPSTREAM
+export NGINX_PORT API_UPSTREAM API_UPSTREAM_HOST
 
-envsubst '${NGINX_PORT} ${API_UPSTREAM}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_PORT} ${API_UPSTREAM} ${API_UPSTREAM_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
